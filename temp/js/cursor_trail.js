a4ecba9b58b1be03fa5b974b61564e931a501fd6
laurajul <l.j.wagner@proton.me>
2026-02-21 19:21:23 +0100

    removed temp directory

--- temp/js/cursor_trail.js
+++
@@ -1,6 +0,0 @@
-const cursor = document.getElementById("custom-cursor");
-
-document.addEventListener("mousemove", (e) => {
-cursor.style.top = `${e.clientY}px`;
-cursor.style.left = `${e.clientX}px`;
-});
--- temp/public/js/loading_screen.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', () => {
-const loading = document.getElementById('loading-screen');
-if (loading) {
-    setTimeout(() => {
-    loading.classList.add('fade-out');
-    setTimeout(() => loading.remove(), 700); // remove from DOM after fade
-    }, 1000); // show for 1 second
-}
-});
